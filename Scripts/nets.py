c1d6ca00e6efa33d4552ac20300abd90dbfc5646
BorDch <bora.cherkasov@outlook.com>
2024-12-12 20:26:14 +0300

    [dev] added new net: VGG and realization in Scripts/nets.py

--- Scripts/nets.py
+++ Scripts/nets.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-
+# Unet
 class SimpleUNet(nn.Module):
     def __init__(self, num_classes):
         super(SimpleUNet, self).__init__()
@@ -29,5 +29,55 @@ class SimpleUNet(nn.Module):
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
+        x = self.classifier(x)
+        return x
+    
+# VGG
+class ImprovedVGG(nn.Module):
+    def __init__(self, num_classes=20):
+        super(ImprovedVGG, self).__init__()
+        self.features = nn.Sequential(
+            # Block 1
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Block 2
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Block 3
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 8 * 8, 512),  # Adjusted for the output size after feature extraction
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(512, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(256, num_classes)  # Output layer for the number of classes
+        )
+
+    def forward(self, x):
+        # print(x.shape)
+        x = self.features(x)
+        x = x.view(x.size(0), -1)  # Flatten the tensor
         x = self.classifier(x)
         return x
